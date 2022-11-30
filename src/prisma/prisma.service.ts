9f63a5924a24ac8857499a337f128bb1e5b11be9
jalallinux <smjjalalzadeh93@gmail.com>
2022-11-30 19:12:50 +0330

    fix db url

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -8,7 +8,7 @@ export class PrismaService extends PrismaClient {
         super({
             datasources: {
                 db: {
-                    url: `${config.get("DB_DRIVER")}://${config.get("DB_USER")}:${config.get("DB_PASS")}@${config.get("DB_HOST")}:${config.get("DB_PORT")}/${config.get("DB_NAME")}?schema=${config.get("DB_SCHEMA")}`,
+                    url: config.get("DATABASE_URL")
                 }
             }
         })
